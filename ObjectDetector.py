686f3229fa65209f619e2fb5bae30605a077705e
zhenggan <zheng-gang.chen@cern.ch>
2025-02-24 08:16:39 +0100

    feat: implement ObjectDetector with Feature Extractor module

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -1,59 +1,28 @@
 import torch
 import torch.nn as nn
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
-from torchvision.utils import draw_bounding_boxes
+from FeatureExtractor import FeatureExtractor as extractor
 import os
-from typing import List
+from typing import List, Tuple
 import cv2
-import time
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as F
 import numpy as np
 os.environ['TORCH_HOME'] = os.getcwd() #will download model weights to your current work directory
 
-from dataset import VideoDataset
 """
 https://pytorch.org/vision/main/auto_examples/others/plot_visualization_utils.html#sphx-glr-auto-examples-others-plot-visualization-utils-py
 """
 
 
-def load_video(video_path: str, resize_shape: List[int] = [224, 224])-> List[torch.Tensor]:
-    frames = []
-    cap = cv2.VideoCapture(video_path)
-    
-    frame_count = 0
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        # Convert BGR to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Resize frame
-        #frame = cv2.resize(frame, resize_shape)
-        # Apply transforms
-        frames.append(torch.from_numpy(frame))
-            
-        frame_count += 1
-        
-    cap.release()
-    
-    # Ensure we have enough frames
-    return frames 
-
-def show(imgs):
-    if not isinstance(imgs, list):
-        imgs = [imgs]
-    fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
-    for i, img in enumerate(imgs):
-        img = img.detach()
-        img = F.to_pil_image(img)
-        axs[0, i].imshow(np.asarray(img))
-        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
-    plt.savefig('123.png')
-
 class ObjectDetector(nn.Module):
     vehicle_label = [2, 3, 4, 6, 8] # class labels for 'car', 'bicycle', 'bus', 'motocycle', 'truck' in FasterRCNN_ResNet50_FPN_Weights  
-    def __init__(self, score_threshold: float = 0.8)->None:
+    def __init__(self, score_threshold: float = 0.8, max_num_objects:int = 20)->None:
+        """
+        Args:
+            score_threshold(float)
+            max_num_objects(int): Maximum number of objects with highest score provided by this object detector.
+        """
         super(ObjectDetector, self).__init__()
         self.weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
         self.model = fasterrcnn_resnet50_fpn(weights = self.weights, progress = False)
@@ -61,6 +30,9 @@ class ObjectDetector(nn.Module):
         self.class_name = self.weights.meta["categories"]
         self.transforms = self.weights.transforms()
         self.score_threshold = score_threshold
+        self.max_num_objects = max_num_objects
+        self.extractor = extractor()
+        self.extractor.eval()
          
     def crop_by_box(self, img, box: List[int]):
         """
@@ -74,56 +46,67 @@ class ObjectDetector(nn.Module):
         for i in range(len(objects)):   
             plt.imshow(objects[i].permute(1, 2, 0).detach().cpu().numpy())
             plt.savefig(f'object-{i:02d}.png') 
-    def process_video(self) -> None:
-        """
-        #Test function#
-        """
-        video_path = './dataset/train/extracted/00227.mp4'
-        frames = load_video(video_path)
-        img = self.transforms(frames[0].permute(2, 0, 1))
-        detection_outputs = self.model(img.unsqueeze(0))
-        output = detection_outputs[0] 
-        score_threshold = .8
-        #vehicle = torch.tensor(['car', 'bicycle', 'person', 'bus', 'motorcycle', 'truck'])
-        class_mask = torch.isin(output['labels'], 
-                    torch.tensor(self.vehicle_label, dtype=torch.int64))
-        
-        score_mask = output['scores'] > score_threshold
-        class_mask = class_mask[score_mask] 
-        boxes = output['boxes'][score_mask] 
-        objects = [self.crop_by_box(img, box) for box in boxes ]
-        #self.plot_object(objects)  -> can use this function to plot the object "cropped" by the faster r-cnn
-        
-        return objects
 
-    def forward(self, x: torch.Tensor) -> None:
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Provide the objects list in each frame using faster R-CNN
         Args: 
-            x: torch.Tensor: Tensor of shape (batch_size, n_frames, channels, height, width)
+            x: torch.Tensor: Tensor of shape (batch_size, channels, height, width)
+        Return:
+            Objects(torch.Tensor), batch_size, num_objects, 4: each box associated with the object given by the faster r-cnn. dimension 4 represent x_min, y_min, x_max, y_max, respectively.
+            Object_mask(torch.Tensor): 0 indicate that there is an object, and 1 indicate that attention mechanism will ignore this position.
         """
+        batch_size = x.shape[0]
         
-        n_frames = x.shape[1]
-        objects = [] 
-        for frame_index in range(n_frames):
-            img = self.transforms(x[:, frame_index, :, :, :].permute(0, 2, 3, 1))    
-            #print(img.shape)
-            img = img.permute(0, 3, 1, 2)
-            detection_outputs = self.model(img) 
-            output = detection_outputs[0]
-            class_mask = torch.isin(output['labels'], 
-                    torch.tensor(self.vehicle_label, dtype=torch.int64))
-            score_mask = output['scores'] > self.score_threshold
-            class_mask = class_mask[score_mask] 
+        images = self.transforms(x.permute(0, 2, 3, 1))    
+        #print(img.shape)
+        images = images.permute(0, 3, 1, 2)
+        detection_outputs = self.model(images)
         
-            boxes = output['boxes'][score_mask][class_mask]
-            print(boxes) 
-            print([self.crop_by_box(img, box) for box in boxes ])
-            #objects.append([self.crop_by_box(img, box) for box in boxes ])
-        ##self.plot_object(objects)  -> can use this function to plot the object "cropped" by the faster r-cnn
-        #return torch.stack(objects, dim = 0).permute()
-        #print()
-        #return objects
+        
+        objects = []
+        objects_mask = []
+        objects_features = []
+        for batch_index in range(batch_size):
+            object_holder = torch.zeros((self.max_num_objects, 4))
+            
+            output = detection_outputs[batch_index]
+
+            class_mask = torch.isin(output['labels'], torch.tensor(self.vehicle_label, dtype=torch.int64))
+            
+            comb_mask = class_mask & (output['scores'] > self.score_threshold) 
+            
+            comb_mask = class_mask & (output['scores'] > self.score_threshold)
+            #raise ValueError() 
+            k = min(comb_mask.sum(), self.max_num_objects) 
+            
+            scores = output['scores'][comb_mask]
+            boxes = output['boxes']
+
+            _, top_k_indices = torch.topk(scores, k = k) 
+            
+            n_objects = len(top_k_indices)
+            
+            boxes = boxes[top_k_indices]
+            object_holder[:n_objects] = boxes
+            
+            obj_mask_holder = torch.ones((self.max_num_objects)) # Will mask the empty position which does not have object. 
+            obj_mask_holder[n_objects:] = 0
+            objects.append(object_holder)
+            objects_mask.append(obj_mask_holder)
+            
+            feature_holder = torch.zeros((self.max_num_objects, 4096))
+            
+            for obj_idx in range(n_objects):
+                print(images[batch_index, :, int(object_holder[obj_idx][1].item()):int(object_holder[obj_idx][3].item()), int(object_holder[obj_idx][0].item()):int(object_holder[obj_idx][2].item())].shape)
+                feature = self.extractor(images[batch_index, :, int(object_holder[obj_idx][1].item()):int(object_holder[obj_idx][3].item()), int(object_holder[obj_idx][0].item()):int(object_holder[obj_idx][2].item())])
+                feature_holder[obj_idx] = feature 
+            objects_features.append(feature_holder) 
+        Objects = torch.stack(objects, dim = 0)
+        Object_features = torch.stack(objects_features, dim = 0)
+        Objects_mask = torch.stack(objects_mask, dim = 0) 
+        
+        return Objects, Object_features, Objects_mask 
 
      
 
@@ -131,6 +114,8 @@ class ObjectDetector(nn.Module):
 
 
 if __name__ == '__main__':
+    from dataset import VideoDataset
+    import time
     #dogs_with_boxes = [
     #    draw_bounding_boxes(img, boxes = boxes[class_mask], width=4)
     #]
@@ -146,9 +131,13 @@ if __name__ == '__main__':
     )
     
     for batch in dataloader:
-        with torch.no_grad():
+        n_frames = batch.shape[1]
+        for frame_idx in range(n_frames): 
+            with torch.no_grad():
+                objects, objects_features, objects_mask = detector(batch[:, frame_idx,:,:,:])
+            #for video in batch:
             #print(batch.shape)
-            detector(batch)
+            #    detector(video[0])
         break
     
     #print(detector.process_video())
