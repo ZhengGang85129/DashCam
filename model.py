56ea7dcd8426c37cf0678d3bf6c733d22d0da71d
zhenggan <zheng-gang.chen@cern.ch>
2025-02-25 03:53:23 +0100

    Add the model script

--- model.py
+++ model.py
@@ -1,19 +1,19 @@
 import torch.nn as nn
 import torch
 from typing import Tuple
-from FeatureExtractor import FeatureExtractor as feature_extractor
 from ObjectDetector import ObjectDetector as object_detector
-
+__all__ = ["LSTM_cell", "DSA_RNN"]
 class LSTM_cell(nn.Module):
-    def __init__(self, input_size:int, hidden_size:int):
+    def __init__(self, input_size:int, hidden_size:int, scale_input = True):
         super(LSTM_cell, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         # Initialize weights for input transformations
-        self.W_i = nn.Parameter(torch.randn(hidden_size, input_size))
-        self.W_f = nn.Parameter(torch.randn(hidden_size, input_size))
-        self.W_c = nn.Parameter(torch.randn(hidden_size, input_size))
-        self.W_o = nn.Parameter(torch.randn(hidden_size, input_size))
+        scale = 2 if scale_input else 1
+        self.W_i = nn.Parameter(torch.randn(hidden_size, input_size * scale))
+        self.W_f = nn.Parameter(torch.randn(hidden_size, input_size * scale))
+        self.W_c = nn.Parameter(torch.randn(hidden_size, input_size * scale))
+        self.W_o = nn.Parameter(torch.randn(hidden_size, input_size * scale))
         
         # Initialize weights for hidden state transformations
         self.U_i = nn.Parameter(torch.randn(hidden_size, hidden_size))
@@ -32,17 +32,23 @@ class LSTM_cell(nn.Module):
         self.b_c = nn.Parameter(torch.zeros(hidden_size))
         self.b_o = nn.Parameter(torch.zeros(hidden_size))
         
+        
+        self.w = nn.Parameter(torch.randn(1, hidden_size))
+        self.W_e = nn.Parameter(torch.randn(hidden_size, hidden_size))
+        self.U_e = nn.Parameter(torch.randn(hidden_size, input_size))
+        self.b_e = nn.Parameter(torch.zeros(hidden_size)) 
+         
         self.init_weights() 
     def init_weights(self,):
         for param in self.parameters():
             if len(param.shape) >= 2:
                 nn.init.xavier_uniform_(param)
-    def forward(self, x: torch.Tensor, state:Tuple[torch.Tensor, torch.Tensor]):
+    def forward(self, feat: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], state:Tuple[torch.Tensor, torch.Tensor]):
         """
         Forward pass of LSTM Cell
         
         Args:
-            x: Input tensor of shape (batch_size, input_size)
+            feat: Input tensor of shape (batch_size, object_size, input_size)
             state: Tuple of (h, c) where both shapes are (batch_size, hidden_size)
             
         Returns:
@@ -50,6 +56,16 @@ class LSTM_cell(nn.Module):
         """
         h, c = state
         
+        x_j, x_mask, X_F = feat 
+        #Attention weight mechanism
+        #print('h: ', h.shape, ', c: ', c.shape, 'W_e: ', self.W_e.shape, ' x_j: ', x_j.shape)
+        e = torch.matmul(torch.tanh(torch.matmul(h, self.W_e.t()) + torch.matmul(x_j, self.U_e.t()) + self.b_e), self.w.t()).squeeze(2)
+        e.masked_fill_(~x_mask, -1e9)
+        a = torch.softmax(e, dim = 1)
+        x_j = torch.einsum('ij, ijk -> ik', a, x_j).unsqueeze(1)
+        
+        X_F = X_F.unsqueeze(1)
+        x = torch.concat((x_j, X_F), dim = 2) 
         # Input gate
         i = torch.sigmoid(
             torch.matmul(x, self.W_i.t()) +
@@ -57,7 +73,6 @@ class LSTM_cell(nn.Module):
             torch.matmul(c, self.V_i.t()) + 
             self.b_i
         )
-        
         # Forget gate
         f = torch.sigmoid(
             torch.matmul(x, self.W_f.t()) +
@@ -77,10 +92,10 @@ class LSTM_cell(nn.Module):
             self.b_o
         )
         
-        
         # New hidden state
         new_h = o * torch.tanh(new_c)
-        
+        #print('new_h: ', new_h.shape)
+        #print('new_c: ', new_c.shape)
         return new_h, new_c
 
 class DSA_RNN(nn.Module):
@@ -94,9 +109,9 @@ class DSA_RNN(nn.Module):
                        hidden_size = self.hidden_size) for Idx in range(num_layers)]
         )
         
-        self.object_detector = object_detector()
-        self.feature_extractor = feature_extractor() 
-        
+        self.object_detector = object_detector(score_threshold = 0.8, max_num_objects = 20)
+        self.object_detector.eval()
+         
     def forward(self, x: torch.Tensor, initial_state: Tuple[torch.Tensor, torch.Tensor] = None):
         """
         Forward pass of stacked LSTM
@@ -110,31 +125,32 @@ class DSA_RNN(nn.Module):
             output: Sequence of hidden states for each time step
             (h_n, c_n): Final hidden and cell states for each layer
         """
-        batch_size, sequence_length = x.shape[0], x.shape[1]
+        batch_size, n_frames = x.shape[0], x.shape[1]
         
         if initial_state is None:
-            h_0 = torch.zeros((batch_size, sequence_length, self.hidden_size), device = x.device)
-            c_0 = torch.zeros((batch_size, sequence_length, self.hidden_size), device = x.device)
+            h_0 = torch.zeros((batch_size,1, self.hidden_size), device = x.device)
+            c_0 = torch.zeros((batch_size, 1, self.hidden_size), device = x.device)
         else:
             h_0, c_0 = initial_state
         
-        h_states = [h_0[i] for i in range(self.num_layers)]
-        c_states = [c_0[i] for i in range(self.num_layers)]
+        h_states = [h_0 for _ in range(self.num_layers)]
+        c_states = [c_0 for _ in range(self.num_layers)]
         
         out_sequence = []
         
-        for t in range(sequence_length):
-            x_t = x[:, t, :]
-            
+        for t in range(n_frames):
+            current_frame = x[:, t, :, :, :]
+            with torch.no_grad():
+                _, objects_features, objects_mask, fullframe_features = self.object_detector(current_frame)
+                
             for layer in range(self.num_layers):
                 if layer == 0:
-                    x_t = x_t
+                    x_t = (objects_features, objects_mask, fullframe_features)
                 else:
                     x_t = h_states[layer-1]
                 h_states[layer], c_states[layer] = self.lstm[layer](
                     x_t, (h_states[layer], c_states[layer]) 
                 )
-            
             out_sequence.append(h_states[-1])
         
         output = torch.stack(out_sequence, dim=1)
