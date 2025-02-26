f4b3aa284598c20babdb3acb9a813b40c802354f
zhenggan <zheng-gang.chen@cern.ch>
2025-02-26 03:03:58 +0100

    feat: description

--- loss.py
+++ loss.py
@@ -5,9 +5,16 @@ class AnticipationLoss(nn.Module):
         super(AnticipationLoss, self).__init__()
         self.decay_nframe = decay_nframe
         self.pivot_frame_index = pivot_frame_index 
-    def forward(self, prob, targets)->torch.Tensor:
+    def forward(self, prob: torch.Tensor, targets: torch.Tensor)->torch.Tensor:
+        """
+        Args: 
+            prob[torch.Tensor, (batch_size, n_frames)]: Scores that the corresponding frame has accident or not.
+            targets[torch.Tensor, (batch_size)]: Indicate whether the corresponding video contains the accident.
+        Return:
+            Loss [torch.Tensor]: Averaged loss.  
+        """
         y = targets.unsqueeze(1)
-        t_step = torch.arange(start = 0, end = prob.shape[1]).unsqueeze(0)#.expand((prob.shape[0], 1))
+        t_step = torch.arange(start = 0, end = prob.shape[1]).unsqueeze(0) 
         loss = - y * torch.exp(- torch.relu(self.pivot_frame_index - t_step)/self.decay_nframe) * torch.log(prob) - (1 - y) * torch.log(prob) 
         loss = loss.mean() 
         return loss 
@@ -16,6 +23,5 @@ if __name__ == "__main__":
     
     output_prob = torch.rand((4, 100))
     target = torch.ones((4))
-     
     Loss = AnticipationLoss()
-    Loss(output_prob, target) 
+    print(Loss(output_prob, target))
