c48ae98d9ccffcf44fc19069ed57e1e8574036a3
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2025-02-28 16:15:19 +0100

    update condor script

--- train.sh
+++ train.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+batch_size=$1
+learning_rate=$2
+
 nvidia-smi
-source /path/to/your/conda/bin/activate # FIXME 
+eval "$(conda shell.bash hook)"
 conda activate dashcam
-cd YOURWORKSPACE # FIXME
-python3 train.py
+cd /eos/user/y/ykao/SWAN_projects/kaggle/DashCam/ # FIXME
+python3 train.py --batch_size ${batch_size} --learning_rate ${learning_rate}
