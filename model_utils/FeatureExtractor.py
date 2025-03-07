d9a16e55bffb2d97edf625f0d213ed3170a2637b
zhenggan <zheng-gang.chen@cern.ch>
2025-03-07 07:05:22 +0100

    Move ObjectDetecotr.py/FeatureExtractor.py to model_utils

--- model_utils/FeatureExtractor.py
+++ model_utils/FeatureExtractor.py
@@ -13,30 +13,33 @@ class FeatureExtractor(nn.Module):
         model = vgg16(weights = VGG16_Weights.DEFAULT) 
         
         self.features = model.features
-        self.classifer = nn.Sequential(
-            *list(model.classifier.children())[:-1]
-        ) 
-        self.transform = transforms.Compose([
-            transforms.Resize([256, 256]),
-            transforms.CenterCrop(size = [224, 224]),
-            transforms.Normalize(mean = [0.485, 0.465, 0.406],
-                                std = [0.229, 0.224, 0.225]
-                                )
-        ])
-        
-         
+        #self.classifer = nn.Sequential(
+        #    *list(model.classifier.children())[:-1]
+        #) 
+        self.transform = VGG16_Weights.DEFAULT.transforms()
+        for param in self.features.parameters():
+            param.requires_grad = False
+        self.classifer = nn.Sequential(*list(model.classifier.children())[:-1]) 
         self.eval()
         
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         '''
         Args:
         x(torch.Tensor, batch_size, channel, width, height)
+        Args:
+        x(torch.Tensor, batch_size, 4096, 224, 224)
         '''
-        x = x.unsqueeze(0)
         x = self.transform(x)
         x = self.features(x)
-        x = torch.flatten(x, 1)
+        x = torch.flatten(x)
         x = self.classifer(x)
         x = x.squeeze(0)
         return x
 
+
+if __name__ == "__main__":
+
+    x = torch.ones((5, 3, 224, 224))
+    model = FeatureExtractor()
+    out = model(x)
+    print(out.shape)
