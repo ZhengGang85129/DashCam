f0ed08fa9fb70d29d4e95c4161f254308bbb5a2e
zhenggan <zheng-gang.chen@cern.ch>
2025-03-13 13:48:00 +0100

    feat: permute the input tensor dimension

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -12,4 +12,16 @@ class baseline_model(nn.Module):
         self.model = r3d_18(weights = R3D_18_Weights.DEFAULT)
         self.model.fc = nn.Linear(self.model.fc.in_features, 2)  # Modify output layer for binary classification
     def forward(self, x:torch.Tensor)->torch.Tensor:
+        '''
+        Args:
+            x (torch.Tensor): batch_size, n_frames, n_channels, height, width
+        Return:
+            y (torch.Tensor): batch_size, n_classes
+        '''
+        x = x.permute(0, 2, 1, 3, 4)
         return self.model(x)
+
+if __name__ == "__main__":
+    
+    model = baseline_model()
+    print(model)
