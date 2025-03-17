f9a2da56358ad5c3ecb597aeefb1a89e806e04dd
zhenggan <zheng-gang.chen@cern.ch>
2025-03-17 16:46:12 +0100

    Introduce adaptive loss function for anticpation

--- utils/loss.py
+++ utils/loss.py
@@ -5,18 +5,17 @@ from typing import Union
 from tool import set_seed
 
 class AnticipationLoss(nn.Module):
-    def __init__(self, decay_nframe:float = 10, pivot_frame_index:float = 100, f2: float = 150, n_frames: int = 100, device: Union[torch.device, None] = None):
+    def __init__(self, frame_of_accident:float = 90, n_frames: float = 100, decay_coefficient: float = 30, device: Union[torch.device, None] = None):
         super(AnticipationLoss, self).__init__()
-        pos_weights = torch.exp(-F.relu(pivot_frame_index  - torch.arange(0, n_frames) - 1) / decay_nframe).view(-1, 1)
-        neg_weights = torch.ones((n_frames, 1))
+        self.frame_of_accident = frame_of_accident
         self.n_frames = n_frames
-        # (n_frames x 2)
-        self.frame_weights = torch.cat([neg_weights, pos_weights], dim=1).unsqueeze(1)
-        self.frame_weights = self.frame_weights.to(device)
-        self.frame_weights.requires_grad = False
-        self.log_softmax = torch.nn.LogSoftmax(dim=2)
-        self.nll_loss = torch.nn.NLLLoss()
-        
+        self.frames = torch.arange(0, n_frames)
+        self.f = decay_coefficient 
+        pos_penalty = torch.exp(-(F.relu(self.frame_of_accident - self.frames)/self.f))
+        neg_penalty = torch.ones([n_frames])
+        self.penalty = torch.stack([neg_penalty, pos_penalty], dim = 1) # shape: [n_frames, 2]
+        self.ce_loss = nn.CrossEntropyLoss(reduction = 'none')
+         
     def forward(self, logits: torch.Tensor, targets: torch.Tensor)->torch.Tensor:
         """
         Args: 
@@ -25,17 +24,25 @@ class AnticipationLoss(nn.Module):
         Return:
             Loss [torch.Tensor]: Averaged loss.  
         """
-        logits = logits.permute(1, 0, 2) # (n_frames, batch_size, 2)
-        loss = self.log_softmax(logits) # (n_frames, batch_size, 2)
-        loss = torch.mul(loss, self.frame_weights) #@ loss
-        loss = loss.view(-1, 2)
-        labels = targets.unsqueeze(1).expand(-1, self.n_frames).reshape(-1).long()
-        loss = self.nll_loss(loss, labels)
-        return loss
-
+        
+        batch_size, n_frames, n_classes = logits.shape
+        targets = F.one_hot(targets, num_classes = n_classes)
+        targets = targets.unsqueeze(1).expand(-1, n_frames, -1).reshape(-1, n_classes)
+        #print(targets.shape)
+        logits = logits.reshape(-1, n_classes)
+        #print(self.penalty.shape) 
+        log_softmax = F.log_softmax(logits)
+        #print(log_softmax.shape)
+        penalty = self.penalty.unsqueeze(0).expand(batch_size, -1, -1).reshape(-1, n_classes)
+        #print(penalty.shape) 
+        weighted_log_softmax = log_softmax * penalty
+        #print(weighted_log_softmax.shape)
+        loss = torch.sum(-weighted_log_softmax * targets)/batch_size/n_frames
+        #print(loss)
 if __name__ == "__main__":
-    set_seed()    
-    output_prob = torch.exp(-torch.ones((16, 100, 2))/0.1)
-    target = torch.ones((16))
+    
+    targets = torch.randint(low = 0, high = 2, size = (10,))
+    targets = targets.to(torch.long)
+    pred = torch.zeros((10, 100, 2))
     Loss = AnticipationLoss()
-    print(Loss(output_prob, target))
+    print(Loss(pred, targets))
