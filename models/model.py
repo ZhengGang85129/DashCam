a7ef11fd0384ff62b453f7b1f30c2df1c5b6123a
zhenggan <zheng-gang.chen@cern.ch>
2025-03-17 10:10:12 +0100

    add: model options

--- models/model.py
+++ models/model.py
@@ -1,2 +1,14 @@
 from .dsa_rnn import DSA_RNN
 from .baseline_model import baseline_model
+from .timesformer import timesformer
+
+def get_model(model_type: str):
+    
+    if model_type == 'baseline':
+        return baseline_model
+    elif model_type == 'timesformer':
+        return timesformer
+    else:
+        raise ValueError(f'No such model yet: {model_type}')
+    
+    
