6202de884ef927ead30db01c54416f14324620c1
zhenggan <zheng-gang.chen@cern.ch>
2025-03-18 06:30:14 +0100

    add: other models

--- models/model.py
+++ models/model.py
@@ -2,14 +2,17 @@ from .dsa_rnn import DSA_RNN
 from .baseline_model import baseline_model
 from .timesformer import timesformer
 from .xai_accident import AccidentXai
+from .swintransformer import swintransformer
 def get_model(model_type: str):
     
-    if model_type == 'baseline':
+    if model_type.lower() == 'baseline':
         return baseline_model
-    elif model_type == 'timesformer':
+    elif model_type.lower() == 'timesformer':
         return timesformer
-    elif model_type == 'AccidentXai':
+    elif model_type.lower() == 'accidentxai':
         return AccidentXai 
+    elif model_type.lower() == 'swintransformer':
+        return swintransformer
     else:
         raise ValueError(f'No such model yet: {model_type}')
     
