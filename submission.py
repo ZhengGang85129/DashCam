65c4dd325b6c90c84d79bb7b2359d7d95d6a6c04
zhenggan <zheng-gang.chen@cern.ch>
2025-03-24 04:02:21 +0100

    refactor: add argument parser

--- submission.py
+++ submission.py
@@ -3,16 +3,32 @@ import torch
 import torch.nn as nn 
 from models.model import baseline_model
 from utils.tool import get_device
+from utils.misc import parse_args
 from utils.Dataset import VideoTo3DImageDataset_Inference
+from utils.accident_inference_dataset import PreAccidentInferenceDataset 
 from typing import Optional
 import torch.nn.functional as F
 import os
 from tqdm import tqdm
+import argparse
+from models.model import get_model
 '''
 kaggle competitions submit -c nexar-collision-prediction -f submission.csv -m "Message"
 '''
+
+def eval_parse_args() -> argparse.ArgumentParser:
+    parser = parse_args(parser_name = 'Evaluation')
+    parser.add_argument('--num_workers', type = int, default = 4, help = 'number of workers to load the data.')
+    parser.add_argument('--model_ckpt', type = str, help = 'model checkpoint path.')
+    parser.add_argument('--model_type', type = str, default = 'baseline')
+    # Augmentation arguments
+    _args = parser.parse_args()
+    return _args
+
+
+
 def load_model(state_path: Optional[str] = None) -> nn.Module:
-    model = baseline_model()
+    model = get_model(args.model_type)
     if state_path is None:
         raise FileNotFoundError(f"The state_dict is None.")
     elif not os.path.isfile(state_path):
@@ -34,21 +50,30 @@ def get_dataloader()-> torch.utils.data.DataLoader:
     Return:
         test dataloader (torch.utils.)
     '''
-    test_dataset = VideoTo3DImageDataset_Inference(
-        root_dir="./dataset/test",
-        csv_file = './dataset/test.csv',
-    )
+    if strategy == 'normal':
+        test_dataset = VideoTo3DImageDataset_Inference(
+            root_dir="./dataset/test",
+            csv_file = './dataset/test.csv',
+        )
+    else:
+        test_dataset = PreAccidentInferenceDataset(
+            root_dir = 'dataset/test',
+            csv_file = 'dataset/test.csv'
+        ) 
     test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size = 16, num_workers = 4, pin_memory = True, shuffle = False
+        test_dataset, batch_size = 16, num_workers = args.num_workers, pin_memory = True, shuffle = False
     )
     return test_loader
 
 
 def main():
-    global device, test_dataloader
+    global device, test_dataloader, strategy, args
+    args = eval_parse_args()
+    strategy = 'SlidingWindow' # Options: UniformSampling/SlidingWindow 
+     
     device = get_device()
     dataloader = get_dataloader()
-    model = load_model(state_path = './first_try/best_model_ckpt_bs16_lr0.001.pt')
+    model = load_model(state_path = args.model_ckpt)
     model = model.to(device)
     results = [] 
     
