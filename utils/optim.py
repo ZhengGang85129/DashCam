f74e8a6ba992feb5b59d5e265fa17eeeaf4af488
zhenggan <zheng-gang.chen@cern.ch>
2025-04-09 15:21:23 +0200

    refactor: revise the optimizer and adding weight decay feature in AdamW

--- utils/optim.py
+++ utils/optim.py
@@ -1,16 +1,32 @@
-
+from typing import Dict, List
+import torch.nn as nn
 import torch.optim.optimizer
 import torch.optim.optimizer
 import torch.optim.sgd
+import torch
 
-
-def get_optimizer(type = str)->torch.optim.Optimizer:
+def get_optimizer(optimizer: Dict, params: List[nn.Parameter])->torch.optim.Optimizer:
     
-    if type.lower() == 'radam':
-        return torch.optim.RAdam
-    elif type.lower() == 'adam':
-        return torch.optim.Adam
-    elif type.lower() == 'sgd':
-        return torch.optim.sgd
+    if optimizer['name'].lower() == 'radam':
+        params_to_be_updated = [p for _, p in params if p.requires_grad]
+        
+        return torch.optim.RAdam(params = params_to_be_updated, lr = float(optimizer['lr']))
+    elif optimizer['name'].lower() == 'adamw':
+        decay = []
+        no_decay = []
+        for name, param in params:
+            if not param.requires_grad:
+                continue
+            if any(nd in name for nd in ["bias", "bn", "norm"]):
+                no_decay.append(param)
+            else:
+                decay.append(param)
+        params_to_be_updated = [
+            {'params': decay, 'weight_decay': float(optimizer['weight_decay'])},
+            {'params': no_decay, 'weight_decay': 0.0}
+        ]
+    
+        
+        return torch.optim.AdamW(params = params_to_be_updated, lr = float(optimizer['lr']))
     
     raise ValueError(f'No such type of optimizer in this repo: {type}.')
