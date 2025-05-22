612d1388d35a027dca12e2d1b24168cb8c13881d
ZhengGang85129 <a0910555246@gmail.com>
2025-05-22 22:11:46 +0800

    Add: Implementation of multiscale vision transformer

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -6,15 +6,33 @@ import os
 from typing import List, Optional
 os.environ['TORCH_HOME'] = os.getcwd() #will download model weights to your current work directory
 
+class LayerNorm3D(nn.Module):
+    def __init__(self, num_features):
+        super(LayerNorm3D, self).__init__()
+        self.layernorm = nn.LayerNorm(num_features)
+    def forward(self, x):
+        
+        b, c, d, h, w = x.size()
+        x = x.view(b, c, -1)
+        x = x.permute(0, 2, 1)
+        x = self.layernorm(x)
+        x = x.permute(0, 2, 1)
+        x = x.view(b, c, d, h, w)
+        return x
+
+
 class baseline_model(nn.Module):
     def __init__(self, classifier: Optional[List] = None):
         super(baseline_model, self).__init__()
         self.backbone = r3d_18(weights = R3D_18_Weights.DEFAULT)
+        
+        backbone_final_in = self.backbone.fc.in_features  
+        self.backbone.fc = nn.Identity()
         if classifier is None:
-            self.backbone.fc = nn.Linear(self.backbone.fc.in_features, 2)  # Modify output layer for binary classification
+            self.classifier = nn.Linear(backbone_final_in, 2)  # Modify output layer for binary classification
         else:
             _classifier_in_list = []
-            prev_infeat = self.backbone.fc.in_features
+            prev_infeat = backbone_final_in
             for elem in classifier:
                 if isinstance(elem, str):
                     if elem.lower() == 'relu':
@@ -28,12 +46,30 @@ class baseline_model(nn.Module):
                 else:
                     raise ValueError(f"Can't assign such layer: {elem}") 
             _classifier_in_list.append(nn.Linear(prev_infeat, 2))
-            self.backbone.fc = nn.Sequential(*_classifier_in_list)
+            self.classifier = nn.Sequential(*_classifier_in_list)
                  
+        #self.replace_batchnorm_with_layernorms(self.backbone)
         self.initial_weights()
         self.freeze()
+    
+    def replace_batchnorm_with_layernorms(self, module):
+        for name, child in module.named_children():
+            if isinstance(child, nn.BatchNorm3d):
+                num_features = child.num_features
+                setattr(module, name, LayerNorm3D(num_features))
+            else:
+                self.replace_batchnorm_with_layernorms(child) 
+      
+    def get_layer_by_name(self, layer_name):
         
-         
+        if hasattr(self.backbone, layer_name):
+            return getattr(self.backbone, layer_name)
+        elif layer_name == 'classifier':
+            return self.classifier
+        else:
+             raise ValueError(f"Layer '{layer_name} not found in model")
+    
+       
     def unfreeze_layers(self, layer_names: List[str]) -> None:
         '''
         Unfreezing the parameters in the corresponding parts.
@@ -41,13 +77,20 @@ class baseline_model(nn.Module):
         
         if "*" in layer_names:
             for param in self.backbone.parameters():
-                param.reguires_grad = True
+                param.requires_grad = True
             return True
         else:
             for layer_name in layer_names:
-                if not hasattr(self.backbone, layer_name):
-                    raise ValueError(f"Layer '{layer_name}' not found in model. Available layers: {list(layer for layer in dir(self.backbone) if not layer.startswith('_'))}")
-                layer = getattr(self.backbone, layer_name)
+                if layer_name == 'classifier':
+                    layer = self.classifier
+                elif hasattr(self.backbone, layer_name):
+                    layer = getattr(self.backbone, layer_name)
+                else:
+                    available = ['classifier'] + [
+                    name for name in dir(self.backbone)
+                    if isinstance(getattr(self.backbone, name), nn.Module) and not name.startswith("_")
+                    ]
+                    raise ValueError(f"Layer '{layer_name}' not found. Available: {available}")
 
                 if not isinstance(layer, torch.nn.Module):
                     raise ValueError(f"The attribute '{layer_name}' exists but is not a nn.Module")
@@ -55,6 +98,7 @@ class baseline_model(nn.Module):
                     param.requires_grad = True
 
         return
+    
     def freeze(self) -> None:
         
         for param in self.backbone.parameters():
@@ -63,7 +107,7 @@ class baseline_model(nn.Module):
      
     def initial_weights(self):
         for name, m in self.named_modules():
-            if isinstance(m, nn.Linear) and name == 'fc':
+            if isinstance(m, nn.Linear) and name.startswith('classifier'):
                 nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
@@ -76,5 +120,6 @@ class baseline_model(nn.Module):
         y: [torch.Tensor, (batch_size, num_classes(2))]
         '''
         x = x.permute(0, 2, 1, 3, 4) 
-        
-        return self.backbone(x)
+        feat = self.backbone(x)
+        out = self.classifier(feat) 
+        return out
--- models/model.py
+++ models/model.py
@@ -1,9 +1,9 @@
-from .dsa_rnn import DSA_RNN
 from .baseline_model import baseline_model
 from .timesformer import timesformer
 from .xai_accident import AccidentXai
 from .swintransformer import swintransformer
 from .model_cnn_lstm import CNN_LSTM
+from .mvit_v2 import mvit_v2
 def get_model(model_type: str):
     
     if model_type.lower() == 'baseline':
@@ -16,6 +16,8 @@ def get_model(model_type: str):
         return swintransformer
     elif model_type.lower() == 'cnn_lstm':
         return CNN_LSTM
+    elif model_type.lower() == 'mvit_v2':
+        return mvit_v2 
     else:
         raise ValueError(f'No such model yet: {model_type}')
     
